fbd020e273675c4ba3bca05606ead68fe865a4a0
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-06-18 15:21:41 +0300

    Updates

---
+++ precache-manifest.2ce6455dde1338ef0769fcd88ac7cd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de91c49ba431dab2c4b2aa174793523",
+    "url": "./index.html"
+  },
+  {
+    "revision": "adf9d58a3a73e0e850fa",
+    "url": "./static/css/main.31ecfe0d.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf9d58a3a73e0e850fa",
+    "url": "./static/js/main.242b5d3c.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
